4cfbf77a8af05713735305ee25e176994024c33d
Alex Leventer <alexleventer@gmail.com>
2021-05-13 12:50:51 -0700

    save wip

--- .hooks/astra/index.js
+++ .hooks/astra/index.js
@@ -3,12 +3,24 @@
 module.exports = strapi => {
     return {
       async initialize() {
+        const { createClient } = require("@astrajs/collections");
+        const astraClient = await createClient({
+          astraDatabaseId: process.env.ASTRA_DB_ID,
+          astraDatabaseRegion: process.env.ASTRA_DB_REGION,
+          applicationToken: process.env.ASTRA_DB_APPLICATION_TOKEN,
+        });
         console.log('my hook is loaded');
         strapi.services.astra = () => {
           return {
             hello: () => {
               return 'hello world';
             },
+            get: () => {
+             // TODO
+            },
+            create: () => {
+              // TODO
+            }
           }
         }
       },
