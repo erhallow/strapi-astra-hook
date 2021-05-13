bec7ab6597e8771a90ceffd9dee89af368c6b35d
erhallow <erhallow@gmail.com>
2021-05-13 08:54:26 -0500

    init commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '45ac1579c1a092de5461a6829162eef2'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'efc43e9e-e91e-4276-af3b-b8b9ffd8f733'
+};
