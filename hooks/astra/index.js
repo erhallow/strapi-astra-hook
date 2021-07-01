d9af494dd2634b0072362f47266f9638d3ffb727
Alex Leventer <alexleventer@gmail.com>
2021-07-01 14:34:13 -0700

    Fix formatting

--- hooks/astra/index.js
+++ hooks/astra/index.js
@@ -1,112 +1,110 @@
-//This is the main javascript which will help you 
+//This is the main javascript which will help you
 //execute get/post/put/delete queries to Astra Cassandra database
-
 
 const request = require('request');
 
 module.exports = strapi => {
-		return {
-			async initialize() {
-				const {
-					token,
-					databaseId,
-					databaseRegion,
-					astraKeyspace,
-				} = strapi.config.get('hook.settings.astra');
+  return {
+    async initialize() {
+      const {
+        token,
+        databaseId,
+        databaseRegion,
+        astraKeyspace,
+      } = strapi.config.get('hook.settings.astra');
 
-				var headers = {
-								'X-Cassandra-Token': token,
-								'Content-Type': 'application/json'
-					};
+      var headers = {
+        'X-Cassandra-Token': token,
+        'Content-Type': 'application/json'
+      };
 
-				var url_ini = 'https://' + databaseId+ '-' +databaseRegion+'.apps.astra.datastax.com/api/rest/v2/namespaces/' +astraKeyspace + '/collections/';
+      var url_ini = 'https://' + databaseId + '-' + databaseRegion + '.apps.astra.datastax.com/api/rest/v2/namespaces/' + astraKeyspace + '/collections/';
 
-				strapi.services.astra = {
+      strapi.services.astra = {
 
-					//creating the document
-					create: async () => {
+        //creating the document
+        create: async () => {
 
-						//This is the sample document data 
-						var dataString = '{ "name": "John", "last_name": "Doe" }';
+          //This is the sample document data
+          var dataString = '{ "name": "John", "last_name": "Doe" }';
 
-						//This is the sample collection name
-						var collection_name = 'employee_detail';
+          //This is the sample collection name
+          var collection_name = 'employee_detail';
 
-						var options = {
-								url: url_ini + collection_name,
-								method: 'POST',
-								headers: headers,
-								body: dataString
-						};
+          var options = {
+            url: url_ini + collection_name,
+            method: 'POST',
+            headers: headers,
+            body: dataString
+          };
 
-						request(options, function(err, res, body) {
-										document_id = JSON.parse(body);
-										console.log(document_id);
-									});
-					},
-
-
-					//Getting the document data using document id
-					getById: async () => {
-
-						var collection_name = 'employee_detail';
-
-						//This is the sample document id
-						var document_id = 'ce7f5f87-225e-41f2-8816-77b4df8d61d1';
-
-						var options = {
-								url: url_ini + collection_name + '/' + document_id ,
-								method: 'GET',
-								headers: headers,
-						};
-						request(options, function(err, res, body) {
-										let json = JSON.parse(body);
-										console.log(json);
-									});
-						},
+          request(options, function (err, res, body) {
+            document_id = JSON.parse(body);
+            console.log(document_id);
+          });
+        },
 
 
-					
-					//Getting the document via document path and returns the most recent entry of data in that specific document
-					getByPath: async () => {
+        //Getting the document data using document id
+        getById: async () => {
 
-						var collection_name = 'employee_detail';
+          var collection_name = 'employee_detail';
 
-						var options = {
-								url: url_ini + collection_name,
-								method: 'GET',
-								headers: headers,
-						};
+          //This is the sample document id
+          var document_id = 'ce7f5f87-225e-41f2-8816-77b4df8d61d1';
 
-						request(options, function(err, res, body) {
-										let json = JSON.parse(body);
-										console.log(json);
-									});
-						},
+          var options = {
+            url: url_ini + collection_name + '/' + document_id,
+            method: 'GET',
+            headers: headers,
+          };
+          request(options, function (err, res, body) {
+            let json = JSON.parse(body);
+            console.log(json);
+          });
+        },
 
 
-					//searching the document via document name
-					searchCollection: async () => {
+        //Getting the document via document path and returns the most recent entry of data in that specific document
+        getByPath: async () => {
 
-							var collection_name = 'employee_detail';
-							// This is the sample query for our document
-							var query = {
-													"name": { "$eq": "John" }
-									}
+          var collection_name = 'employee_detail';
 
-						var options = {
-								url: url_ini+collection_name+'?where='+JSON.stringify(query)+'&page-size=3',
-								method: 'GET',
-								headers: headers,
-						};
+          var options = {
+            url: url_ini + collection_name,
+            method: 'GET',
+            headers: headers,
+          };
 
-						request(options, function(err, res, body) {
-										let json = JSON.parse(body);
-										console.log(json);
-									});
-						},
+          request(options, function (err, res, body) {
+            let json = JSON.parse(body);
+            console.log(json);
+          });
+        },
 
-				}
-			},
-		};
-	};
+
+        //searching the document via document name
+        searchCollection: async () => {
+
+          var collection_name = 'employee_detail';
+          // This is the sample query for our document
+          var query = {
+            "name": {"$eq": "John"}
+          }
+
+          var options = {
+            url: url_ini + collection_name + '?where=' + JSON.stringify(query) + '&page-size=3',
+            method: 'GET',
+            headers: headers,
+          };
+
+          request(options, function (err, res, body) {
+            let json = JSON.parse(body);
+            console.log(json);
+          });
+        },
+
+      }
+    },
+  };
+};
