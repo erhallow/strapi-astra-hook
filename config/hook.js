fc490b876dbd945fe69e507ee062b93a47060052
Disha Patel <disha.patel@datastax.com>
2021-07-07 16:21:18 -0500

    updated code

--- config/hook.js
+++ config/hook.js
@@ -9,8 +9,7 @@ module.exports = {
       token: process.env.ASTRA_TOKEN,
       databaseId: process.env.ASTRA_DB_ID,
       databaseRegion: process.env.ASTRA_DB_REGION,
-      keyspace: process.env.ASTRA_DB_KEYSPACE,
-      collection: process.env.ASTRA_DB_COLLECTION,
+      keyspace: process.env.ASTRA_DB_KEYSPACE
     },
   },
 };
